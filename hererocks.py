1ce0f8ab4ca1f5acdcc9d23b48992d92bdebefe2
Carl Smedstad <carl.smedstad@protonmail.com>
2021-07-05 18:23:13 +0200

    Use HTTPS when downloading Lua & LuaRocks

--- hererocks.py
+++ hererocks.py
@@ -899,8 +899,8 @@ class Patch(object):
 class RioLua(Lua):
     name = "lua"
     title = "Lua"
-    base_download_urls = ["http://www.lua.org/ftp", "http://webserver2.tecgraf.puc-rio.br/lua/mirror/ftp"]
-    work_base_download_url = "http://www.lua.org/work"
+    base_download_urls = ["https://www.lua.org/ftp", "https://webserver2.tecgraf.puc-rio.br/lua/mirror/ftp"]
+    work_base_download_url = "https://www.lua.org/work"
     default_repo = "https://github.com/lua/lua"
     versions = [
         "5.1", "5.1.1", "5.1.2", "5.1.3", "5.1.4", "5.1.5",
@@ -2008,7 +2008,7 @@ class RaptorJIT(LuaJIT):
 class LuaRocks(Program):
     name = "luarocks"
     title = "LuaRocks"
-    base_download_url = "http://luarocks.github.io/luarocks/releases"
+    base_download_url = "https://luarocks.github.io/luarocks/releases"
     default_repo = "https://github.com/luarocks/luarocks"
     versions = [
         "2.0.8", "2.0.9", "2.0.10", "2.0.11", "2.0.12", "2.0.13",
