50da40439bdf958e8361fc040d7e508ddf54748c
Francois Perrad <francois.perrad@gadz.org>
2021-11-08 20:26:22 +0100

    Add support for LuaRocks 3.8.0

--- hererocks.py
+++ hererocks.py
@@ -2009,6 +2009,7 @@ class LuaRocks(Program):
         "3.5.0",
         "3.6.0",
         "3.7.0",
+        "3.8.0",
     ]
     translations = {
         "2": "2.4.4",
@@ -2026,8 +2027,9 @@ class LuaRocks(Program):
         "3.5": "3.5.0",
         "3.6": "3.6.0",
         "3.7": "3.7.0",
-        "^": "3.7.0",
-        "latest": "3.7.0"
+        "3.8": "3.8.0",
+        "^": "3.8.0",
+        "latest": "3.8.0"
     }
     checksums = {
         "luarocks-2.0.10.tar.gz"   : "11731dfe6e210a962cb2a857b8b2f14a9ab1043e13af09a1b9455b486401b46e",
@@ -2100,6 +2102,8 @@ class LuaRocks(Program):
         "luarocks-3.6.0-win32.zip" : "ac9cc1626c4c6171e2cc9259e788934737387c76578775769986924c21b6c590",
         "luarocks-3.7.0.tar.gz"    : "9255d97fee95cec5b54fc6ac718b11bf5029e45bed7873e053314919cd448551",
         "luarocks-3.7.0-win32.zip" : "bfd88072a9f8496839e9256735cd0b6a5a439ac9088e4dfbdc87e21658115504",
+        "luarocks-3.8.0.tar.gz"    : "56ab9b90f5acbc42eb7a94cf482e6c058a63e8a1effdf572b8b2a6323a06d923",
+        "luarocks-3.8.0-win32.zip" : "9cae5f3f61d49be2a8ee8eee124830c3d8012a9e3de57b119de3de626d3017fd",
     }
 
     def get_download_name(self):
@@ -2528,8 +2532,8 @@ def main(argv=None):
     parser.add_argument(
         "-r", "--luarocks", help="Version of LuaRocks to install. "
         "Version can be specified in the same way as for standard Lua. "
-        "Versions 2.0.8 - 3.7.0 are supported. "
-        "'latest' and '^' are aliases for 3.7.0. "
+        "Versions 2.0.8 - 3.8.0 are supported. "
+        "'latest' and '^' are aliases for 3.8.0. "
         "Default git repo is https://github.com/luarocks/luarocks. "
         "Note that Lua 5.2 is not supported in LuaRocks 2.0.8, "
         "Lua 5.3 is supported only since LuaRocks 2.2.0, Lua 5.4 is supported only since "
